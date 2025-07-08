248af25be8401904454fea8e1976214d68c75035
kdjnv <phongboy2003fa@gmail.com>
2025-07-08 16:17:29 +0700

    add memory

--- CPU_RV32I/ALU_unit.v
+++ CPU_RV32I/ALU_unit.v
@@ -1,21 +1,21 @@
 module ALU_unit(
-    input           rst,
     input           clk,
 
     input           isALUimm,
     input           isALUreg,
     input           isBranch,
-    input   [3:0]   funct3b,
     input   [7:0]   funct3oh,
     input   [6:0]   funct7,
     input   [31:0]  rs1,
     input   [31:0]  rs2,        //Imm 
     
-    output  [31:0]  result
+    output  [31:0]  result,
+    output          correct
 );
 
 //ALU 
 wire    isALU   =   isALUimm|   isALUreg;
+
 wire    ADD     =   isALU   &   funct3oh[0] &   !funct7[5]; //ADDI or ADD
 wire    SUB     =   isALUreg&   funct3oh[0] &   funct7[5];  //SUB
 wire    AND     =   isALU   &   funct3oh[7];                //ANDI or AND
@@ -27,30 +27,33 @@ wire    SRA     =   isALU   &   funct3oh[5] &   funct7[5];  //SRAI or SRA
 wire    SLT     =   isALU   &   funct3oh[2];                //SLTI or SLT
 wire    SLTIU   =   isALU   &   funct3oh[3];                //SLTIU or SLTU
 
+//Sign for compare
+wire   isSigned    =   !(funct3oh[7] | funct3oh[6]);
+
 //Compare
-wire    CP  =   rs1 < rs2;
-wire    CS  =   (rs1[31] ^ rs2[31])?rs1[31]:CP;//Compare signed; 1 = LT; 0 = LT
-wire    EQ  =   rs1 == rs2;
-wire    LT  =   isSigned?CS:CP;
-wire    GE  =   !LT;
+wire    CP  =   rs1 < rs2;                      //Compare unsigned
+wire    CS  =   (rs1[31] ^ rs2[31])?rs1[31]:CP; //Compare signed; 1 = LT; 0 = LT
+wire    EQ  =   rs1 == rs2;                     //Equal
+wire    LT  =   isSigned?CS:CP;                 //Less than
+wire    GE  =   !LT;                            //Greater than
 
 //correct branch
 assign  correct =    
-            funct3b[0]  &   EQ  |
-            funct3b[1]  &  !EQ  |
-            funct3b[2]  &   LT  |
-            funct3b[3]  &   GE;
+            funct3oh[0]             &   EQ  |   //is BEQ 
+            funct3oh[1]             &  !EQ  |   //is BNE
+           (funct3oh[4]|funct3oh[6])&   LT  |   //is BLT or BLTU
+           (funct3oh[5]|funct3oh[7])&   GE;     //is BGE or BGEU
 
-//result
-assign  result  =   ADD ?   in1 +   in2:
-                    SUB ?   in1 -   in2:
-                    AND ?   in1 &   in1:
-                    OR  ?   in1 |   in2:
-                    XOR ?   in1 ^   in2:
-                    SLL ?   in1 <<  in2:
-                    SRL ?   in1 >>  in2:
-                    SRA ?   
+//result of ALU
+assign  result  =   ADD ?   rs1 +   rs2:
+                    SUB ?   rs1 -   rs2:
+                    AND ?   rs1 &   rs2:
+                    OR  ?   rs1 |   rs2:
+                    XOR ?   rs1 ^   rs2:
+                    SLL ?   rs1 <<  rs2:        //note shami (SLLI)
+                    SRL ?   rs1 >>  rs2:        //note shami (SRLI)
+                    SRA ?   (rs1 >> rs2)|((~(32'hffffffff>>rs2))&{32{rs1[31]}}):        //note shami (SRAI)
                     SLT ?   (CS?32'd1:32'd0):
-                    SLTU?   (CP?32'd1:32'd0);        
+                    SLTIU?  (CP?32'd1:32'd0):32'd0;        
 
 endmodule
